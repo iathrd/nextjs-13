6326b12080585b3321f15c7cd6a382c4ffe6d297
Iqbal Athorid <iqbalathoridd@gmail.com>
2024-02-04 16:40:06 +0700

    implement left and right sidebar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,38 @@
-import { UserButton } from "@clerk/nextjs";
+import HomeFilters from "@/components/home/HomeFilters";
+import Filter from "@/components/shared/Filter";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
+import Link from "next/link";
 import React from "react";
 
 function Home() {
   return (
-    <header>
-      <UserButton afterSignOutUrl="/" />
-    </header>
+    <>
+      <div className="flex w-full flex-col-reverse justify-between sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link className="flex justify-end max-sm:w-full" href="/ask-question">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPostion="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+      <HomeFilters />
+    </>
   );
 }
 
